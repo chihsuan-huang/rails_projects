66fd280782ac5a0750167d8504afc89d64e1e30e
Chi-Hsuan <chihsuan.huang.7@gmail.com>
2014-04-22 17:54:55 +0800

    Finish user signup

--- microblog_app/spec/models/user_spec.rb
+++ microblog_app/spec/models/user_spec.rb
@@ -3,30 +3,35 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com")
-  end
+      @user = User.new(name: "Example User", email: "user@example.com")
+    end
 
   subject { @user }
-  it { should respond_to(:name) }
+
+   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should be_valid }
-
-
   it { should be_valid }
 
   describe "when name is not present" do
-    before { @user.name = " " }
+      before { @user.name = " " }
+      it { should_not be_valid }
+    end
+    
+   describe "when email is not present" do
+    before { @user.email = " " }
     it { should_not be_valid }
   end
 
-   describe "when name is too long" do
+    describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
+  end
 
-    describe "when email format is invalid" do
+   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
@@ -45,17 +50,10 @@ describe User do
         expect(@user).to be_valid
       end
     end
-
-     describe "when email address is already taken" do
-    before do
-      user_with_same_email = @user.dup
-      user_with_same_email.save
-    end
-
-    it { should_not be_valid }
+  end
 
 
-     describe "when email address is already taken" do
+  describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
@@ -64,8 +62,6 @@ describe User do
 
     it { should_not be_valid }
   end
-  end
-  end  
 
     describe "when password is not present" do
     before do
@@ -81,7 +77,7 @@ describe User do
   end
 
 
-    describe "with a password that's too short" do
+   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
@@ -102,3 +98,7 @@ describe User do
     end
   end
 end
+
+
+
+
