af271eb226abca96a9b78bf69431ea08e14a00f8
Chi-Hsuan <chihsuan.huang.7@gmail.com>
2014-04-25 22:48:48 +0800

    Add user microposts

--- microblog_app/app/models/user.rb
+++ microblog_app/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-    before_save { self.email = email.downcase }
+  has_many :microposts, dependent: :destroy
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
@@ -11,6 +12,11 @@ class User < ActiveRecord::Base
 
    def User.new_remember_token
     SecureRandom.urlsafe_base64
+  end
+
+    def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
   end
 
   def User.hash(token)
